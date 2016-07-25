1ab9228fd7004d1d1d1fb93c83301caa7ef1f344
Reginald Tan <redge.tan@gmail.com>
2016-07-24 23:27:05 -0400

    init

---
+++ Bard/storage/UserConfig.swift
@@ -0,0 +1,17 @@
+//
+//  UserSettings.swift
+//  Bard
+//
+//  Created by Reginald Tan on 2016-07-24.
+//  Copyright © 2016 ROP Labs. All rights reserved.
+//
+
+import Foundation
+
+class UserConfig {
+    
+    static func isLogined() -> Bool {
+        return true
+    }
+    
+}
---
+++ Bard/ui/RepositoriesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bard
+//
+//  Created by Reginald Tan on 2016-07-24.
+//  Copyright © 2016 ROP Labs. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
