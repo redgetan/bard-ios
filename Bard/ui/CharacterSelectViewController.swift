3828870601fee92a02ba6f9ea90347914acd6e8f
Reginald Tan <redge.tan@gmail.com>
2016-07-26 15:51:29 -0400

    dismiss choose character

---
+++ Bard/ui/CharacterSelectViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CharacterSelectViewController.swift
+//  Bard
+//
+//  Created by Reginald Tan on 2016-07-26.
+//  Copyright © 2016 ROP Labs. All rights reserved.
+//
+
+import UIKit
+
+class CharacterSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+}
