c6c6f29fc373fe0326516f7b5104426c72b533fd
Reginald Tan <redge.tan@gmail.com>
2017-03-23 14:11:52 -0400

    upload viewcontroller

---
+++ Bard/ui/UploadViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UploadViewController.swift
+//  Bard
+//
+//  Created by Reginald Tan on 2017-03-23.
+//  Copyright © 2017 ROP Labs. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+    
+    @IBOutlet weak var progressResultView: UIView!
+    @IBOutlet weak var uploadFormView: UIView!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    
+    
+    override func viewWillDisappear(animated: Bool)
+    {
+        super.viewWillDisappear(animated)
+    }
+    
+    
+    
+}
